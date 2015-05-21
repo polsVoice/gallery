ee1ca6984663ca89706f2488e586fd17410b848b
Brent Bartlett <brent.bartlett1@gmail.com>
2015-05-20 18:37:17 -0700

    insert directory names into menu

--- public/index.js
+++ public/index.js
@@ -2,6 +2,11 @@ var gallery = {
     init: function(){
         $.get( "/directories", function( data ){
             console.log( data );
+            $.each( data, function( index, value ){
+                console.log( index + ": " + value );
+                $( "#menu" ).append( "<option value=" +
+                    value + ">" + value + "</option>" );
+            } );
         } );
     }
 };
