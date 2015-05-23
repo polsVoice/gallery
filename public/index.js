75fc5b1c6cf4c9de7870ae04d6c1eb325ea3f98b
Brent Bartlett <brent.bartlett1@gmail.com>
2015-05-23 10:51:33 -0700

    server prints directory contents

--- public/index.js
+++ public/index.js
@@ -1,12 +1,34 @@
 var gallery = {
+    getFileList: function(){
+        "use strict";
+        var dir = $( this ).val();
+        console.log( dir );
+        $.ajax( {
+            url: "/dirList",
+            type: "POST",
+            data: {"dir":dir},
+            datatype: "json",
+            success: function( data ){
+                console.log( data);
+            },
+            error: function( jqxhr, status, errThrown ){
+                console.log( jqxhr.responseText );
+                console.log( status );
+                console.log( errThrown );
+            }
+        } );
+    },
     init: function(){
+        "use strict";
+        var opt;
         $.get( "/directories", function( data ){
-            console.log( data );
             $.each( data, function( index, value ){
+                opt = "<option value=" +
+                    value + ">" + value + "</option>";
                 console.log( index + ": " + value );
-                $( "#menu" ).append( "<option value=" +
-                    value + ">" + value + "</option>" );
+                $( "#menu" ).append( opt );
             } );
+            $( "#menu" ).change( gallery.getFileList );
         } );
     }
 };
--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 ( function(){
     var fs = require( "fs" ),
         express = require( "express" ),
+        bodyParser = require( "body-parser" ),
         app = express();
 
+    var walk = function(dir, done) {
+        var results = [];
+        fs.readdir(dir, function(err, list) {
+            if (err) {return done(err)};
+            var i = 0;
+            (function next() {
+                var file = list[i++];
+                if (!file) return done(null, results);
+                file = dir + '/' + file;
+                fs.stat(file, function(err, stat) {
+                    if (stat && stat.isDirectory()) {
+                      walk(file, function(err, res) {
+                        results = results.concat(res);
+                        next();
+                      });
+                    } else {
+                      results.push(file);
+                      next();
+                    }
+                });
+            })();
+        });
+    };
+    app.use( bodyParser.urlencoded( { extended: false } ) );
     fs.readFile( __dirname + "/test.txt", function( err, data ){
-        if ( err ){
-            throw err;
-        }
+        if ( err ){throw err;}
         console.log( data.toString() );
     } );
 
     app.get( "/", function( req, res ){
+        "use strict";
         var html = fs.readFileSync( __dirname + "/public/index.html" );
         res.writeHeader( 200, { "Content-Type": "text/html" } );
         res.write( html );
@@ -18,6 +42,7 @@
     } );
 
     app.get( "/directories", function( req, res ){
+        "use strict";
         fs.readdir(__dirname, function(err, files) {
             if (err) {return;}
             var arr = [];
@@ -34,38 +59,16 @@
         });
     } );
 
+    app.post( "/dirList", function( req, res ){
+        var dir = req.body.dir;
+        console.log( req.body.dir );
+        walk( process.env.PWD + "/" + dir, function( err, results ){
+            if (err ){ throw err; }
+            var arr = [];
+            console.log( results );
+        } );
+    } );
 
-    var walk = function(dir, done) {
-      var results = [];
-      fs.readdir(dir, function(err, list) {
-        if (err) return done(err);
-        var i = 0;
-        (function next() {
-          var file = list[i++];
-          if (!file) return done(null, results);
-          file = dir + '/' + file;
-          fs.stat(file, function(err, stat) {
-            if (stat && stat.isDirectory()) {
-              walk(file, function(err, res) {
-                results = results.concat(res);
-                next();
-              });
-            } else {
-              results.push(file);
-              next();
-            }
-          });
-        })();
-      });
-    };
-    //~ walk( process.env.PWD, function( err, results ){
-        //~ if ( err ){
-            //~ throw err;
-        //~ }
-        //~ var arr = [];
-        //~ console.log( results );
-    //~ } );
-    
     app.use( "/", express.static( __dirname + "/public/" ) );
 
     app.listen( 30025 );
