f3b089b7ac076235622ecb3e05c954a6497531dc
Brent Bartlett <brent.bartlett1@gmail.com>
2015-10-02 22:44:24 -0700

    trying a different pattern

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-var gallery = {
-    fs: require( "fs" ),
-    express: require( "express" ),
-    bodyParser: require( "body-parser" ),
-    walk: function(callback){
-        "use strict";
-        var nodeDir = require( "node-dir" );
-        nodeDir.files( "./public/img", function( err, files ){
-            if ( err ){ throw err; }
-            callback( files );
-        } );
-    },
-    init: function(){
-        var app = this.express();
-        app.use( this.bodyParser.urlencoded( { extended: false } ) );
-        app.get( "/", function( req, res ){
-            res.send( "Hello world" );
-        } );
-        app.listen( 3000 );
-    }
-};
-//~ gallery.init();
-module.exports = gallery;
+module.exports = function(){
+    var fs = require( "fs" )
+        , express = require( "express" )
+        , bodyParser = require( "body-parser" )
+        , nodeDir = require( "node-dir" )
+        , app = express();
+    return {
+        walk: function( callback ){
+            "use strict";
+            nodeDir.files( "./public/img", function( err, files ){
+                if ( err ){ throw err; }
+                callback( files );
+            } );
+        },
+        init: function(){
+            app.get( "/", function( req, res ){
+                res.send( "hello world" );
+            } );
+            app.listen( 3000 );
+        }
+    };
+}();
